a2d4b6c278033a3c99c92eb95f092683a04b0d07
Derek Hamel <dbhamel@asu.edu>
2015-09-04 17:44:11 -0700

    initial pages implemented

--- app/src/main/java/edu/asu/aspira/MainActivity.java
+++ app/src/main/java/edu/asu/aspira/MainActivity.java
@@ -1,20 +1,43 @@
 package edu.asu.aspira;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Button startBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+
         Intent intent = new Intent(this, PushService.class);
         this.startService(intent);
+
+        startBtn = (Button)findViewById(R.id.startBtn);
+
+        startBtn.setTypeface(Typeface.createFromAsset(getAssets(), "LuckiestGuy.ttf"));
+
+        MediaPlayer player = new MediaPlayer();
+        try {
+            player.setDataSource(this, Uri.parse("android.resource://edu.asu.aspira/raw/" + R.raw.star_wars));
+            player.prepare();
+            player.start();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -37,5 +60,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onStartClicked(View v){
+        Intent intent = new Intent(this, Introduction.class);
+        startActivity(intent);
+
     }
 }
